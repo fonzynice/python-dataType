23d77564517670f6198bc9c7ea986e0051b17ee8
fonzynice <fonzynice@gmail.com>
2019-01-25 08:05:49 -0500

    file updated with new code

--- DataType.py
+++ DataType.py
@@ -1,6 +1,9 @@
-'''
+from array import *
+import os, sys
+
+"""
 This is a docstring tutorials!
-'''
+"""
 
 myInt = 9
 
@@ -12,7 +15,7 @@ myString = 'cisco'
 
 stringLiteral = 'network programming and linux'
 
-myList = ['macintosh', 'processor', 'cpu', 'ram', 'hdd', 'video']
+myList = ['macintosh', 'processor', 'cpu', 'ram', 'hdd', 'video', 'batt']
 
 myTuple = (1, 4, 'IOS', 'NX-OS')
 
@@ -23,13 +26,70 @@ myDict = {'make': 'Apple',''
           'protocol': 'OSPF'
           }
 
+# def greet(*names):
+#     """This function greets all
+#        the person in the names tuple."""
+#     # names is a tuple with arguments
+#     for name in names:
+#         print("Hello", name)
 
-def greet(*names):
-    """This function greets all
-       the person in the names tuple."""
-    # names is a tuple with arguments
-    for name in names:
-        print("Hello", name)
+# greet("Monica","Luke","Steve","John")
+
+# myList2 = myList.copy()
+# print(myList2)
+#
+# for names in myList:
+#     print(names)
+#
+# import fractions
+#
+# # Output: 3/2
+# print(fractions.Fraction(1.5))
+
+# for i in range(4):
+#     for j in range(4):
+#         print('#', end='')
+#     print()
+
+# print all Exception error
+# print(locals()['__builtins__'])
 
 
-greet("Monica","Luke","Steve","John")
+# class Error(Exception):
+#     """" Base class for my custom Exception """
+#     pass
+#
+#
+# arr = array('i',[])
+#
+# try:
+#     number = int(input('Enter length of your array: '))
+#
+#     for i in range(number):
+#         user_input = int(input('Now enter integer number: '))
+#         arr.append(user_input)
+#     print('\nTotal Array is: ',arr)
+#
+# except AttributeError as e:
+#     print('Please import the proper array module', e)
+#
+# except (ValueError, AttributeError):
+#     print('\nPlease enter Integer number: Process Aborted')
+#
+# finally:
+#     print('\n.........')
+#
+#
+
+# OS = os.rmdir('/Users/apple/testDirectory')
+#
+# print(OS)
+# print(os.listdir())
+
+
+with open('text.txt', mode='r+', encoding='utf-8') as f:
+    print(f.read())
+    f.write('\nPlease make sure you check out cisco"s Devnet website ')
+    print(f.read())
+
+
